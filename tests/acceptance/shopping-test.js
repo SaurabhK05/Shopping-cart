6f1deee27f34aa87f299374d1ad1809684cb1e51
SaurabhK05 <saurabh.k150600@gmail.com>
2022-01-06 19:49:40 +0530

    ..

--- tests/acceptance/shopping-test.js
+++ tests/acceptance/shopping-test.js
@@ -12,16 +12,16 @@ module('Acceptance | shopping', function (hooks) {
     await click('[data-test-product="1"]');
 
     assert.strictEqual(currentURL(), '/items/1');
-    // await click('[data-test-add-to-cart]');
+    await click('[data-test-add-to-cart]');
 
-    // await click('[data-test-add-to-cart-button]');
+    await click('[data-test-add-to-cart-button]');
 
-    // assert.strictEqual(currentURL(), '/shopping-cart');
+    assert.strictEqual(currentURL(), '/shopping-cart');
 
-    // assert.dom('h4').hasText("Beats Solo Wireless Headphones");
+    assert.dom('h4').hasText("Beats Solo Wireless Headphones");
 
-    // assert.dom('i').hasText("$99.98");
+    assert.dom('i').hasText("$99.98");
     
-    await this.pauseTest();
+    // await this.pauseTest();
   });
 });
